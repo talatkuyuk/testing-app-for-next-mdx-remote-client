125682249ca3afd80da1a051bae4f1b8e05caf83
talatkuyuk <talatkuyuk@gmail.com>
2025-11-27 22:47:43 +0300

    Update next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
 };
 
 export default nextConfig;
